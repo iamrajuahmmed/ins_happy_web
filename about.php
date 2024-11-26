3575c14ddcbd446e7f23deddd6a7b857503ba6a1
Raju Ahmmed <147970303+iamrajuahmmed@users.noreply.github.com>
2024-11-26 16:15:30 +0600

    Add files via upload

---
+++ about.php
@@ -0,0 +1,44 @@
+<?php
+    include('header.php')
+?>
+<section class="about_us_header">
+    <div class="about_us_main">
+        <div class="about_us_header_text">
+            <h1><a href="index.php">Home</a> > <a href="about.php">About Us</a></h1>
+        </div>
+    </div>
+
+</section>
+
+<section class="about_us">
+    <div class="about_Description">
+        <h1>About us</h1>
+        <div class="about_text">
+            <div class="about_main_text">
+                <p>We are inspired by the Kingdom of Bhutan’s philosophy of Gross National Happiness (GNH). The Institute of Happiness (IOH) is an evidence-based think and do tank that seeks to be a thought center for Bhutanese GNH experience. The institute will serve as an accumulator for GNH philosophy and present the alternative development paradigm for actors in societal structure for harmony .
+                    <br><br>
+                 The IOH’s objectives are to:</p>
+                 <ul>
+                    <li>Conduct and facilitate collaborative research with institutions/ partners</li>
+                    <li>Develop, Assess and Impart training/skills for actors in the policy process including leaders from Government, Business and Civil Societies.</li>
+                    <li>Organize, coordinate and lead experiential trips to learn, understand and discuss the Bhutanese GNH journey.</li>
+                 </ul>
+                 <h2>Our Story</h2>
+                 <p>The GNH philosophy originated in Bhutan, but has inspired scholars, academics, and policymakers around the world. The IOH shall be a private intellectual platform where academics and practitioners of GNH philosophies can convene to discuss, learn, and implement best practices in development, governance, policy, and business management. <br><br>
+
+                    The IOH has three primary practice areas: <a href="traning.php">Training</a>, <a href="research.php">Research</a> and <a href="collaboration.php">Collaboration</a>. <br><br>
+                    Interested in <a href="signup.php">joining us</a> ? View open positions with the IOH.</p>
+
+            </div>
+            <div class="about_main_image">
+                <img src="image/about_us.png" alt="">
+
+            </div>
+        </div>
+    </div>
+
+</section>
+
+<?php
+    include('footer.php')
+?>
